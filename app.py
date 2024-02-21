6844c26c361fe1a8ebf9d76b40c60a7bb2e6f9c3
Kilaketia <kilaketia@pm.me>
2024-02-21 10:44:07 +0100

    trying to make it working

--- app.py
+++ app.py
@@ -3,14 +3,45 @@ import json
 import sys
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+import mysql.connector
 
-#MESSAGE = os.environ.get('MESSAGE')
-with open('/volume/message.txt') as f:
-    try:
-        MESSAGE = f.readline()
-    except:
-        print("Lecture du fichier impossible !")
 APP_PORT = os.environ.get('APP_PORT')
+
+mydb = mysql.connector.connect(
+  host="mariadb-service.killian-page.svc.cluster.local",
+  user="root",
+  password="admin"
+)
+
+cursor = mydb.cursor()
+
+cursor.execute("show databases;")
+for db in cursor:
+    if db == "configs":
+        dbCreated = True
+    else: 
+        dbCreated = False
+    
+if dbCreated: 
+    cursor.execute("USE configs")
+    cursor.execute("SELECT value FROM keysta WHERE keyname='message'")
+    result = cursor.fetchone()
+    if result:
+        MESSAGE = result
+        return result  # Return the value of 'message'
+    else:
+        return None  # If 'message' entry doesn't exist
+else: 
+    cursor.execute("CREATE DATABASE IF NOT EXISTS configs")
+    cursor.execute("USE configs")
+    cursor.execute('''CREATE TABLE IF NOT EXISTS keysta
+                     (keyname VARCHAR(255) PRIMARY KEY, value VARCHAR(255))''')
+    cursor.execute('INSERT INTO keysta VALUES ("message","DB works")')
+    mydb.commit()
+    MESSAGE = "Just been initialized"
+
+cursor.close()
+mydb.close()
 
 # creating the flask app
 app = Flask(__name__)
